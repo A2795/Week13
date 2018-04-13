e4366c9b91e5573a086ba609570e06cd7c4fc086
A2795 <35545665+A2795@users.noreply.github.com>
2018-04-13 05:00:08 +0000

    Chapter 12 Script

---
+++ Week12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "lets obtain yourlocal_ip_address"
+#ifconfig
+sleep 1
+echo "lets create a simpleHTTPserver"
+echo "lets open a webbrowser so you type following command... yourlocal_ip_addr$
+sleep 3
+firefox
+echo "the simpleHTTPserver will now run, access any file and check the activity$
+python -m SimpleHTTPServer
+
+#end of script
+
